45c2f20c1aefb738955a22e15d15cf2df0286327
Roman Kolpak <kolpak.roman@gmail.com>
2021-01-05 15:42:39 +0200

    Clean up interface and implement missing bits

--- pymongo_cursor_pager/find.py
+++ pymongo_cursor_pager/find.py
@@ -2,8 +2,6 @@ import pymongo
 from dataclasses import dataclass
 from typing import Optional, Iterable
 
-from bson import ObjectId
-
 from pymongo.collection import Collection
 
 from pymongo_cursor_pager.encode import decode_cursor, encode_cursor
@@ -17,27 +15,24 @@ class PaginatedResult:
     has_next: bool
 
 
-@dataclass
-class PaginationOptions:
-    limit: int
-    next: Optional[str]
-    previous: Optional[str]
-    sort: Iterable[tuple]
+def get_pagination_query(next_cursor: Optional[str], previous_cursor: Optional[str]):
+    if next_cursor:
+        return decode_cursor(next_cursor)
 
-
-def get_pagination_query(options):
-    if options.next:
-        return decode_cursor(options.next)
-
-    if options.previous:
-        return decode_cursor(options.previous)
+    if previous_cursor:
+        return decode_cursor(previous_cursor)
 
     return {}
 
 
-def get_next_cursor(query_result: list, options: PaginationOptions) -> dict:
-    last_item = query_result[-1]
-    sorted_by = options.sort
+def get_next_cursor(query_result: list, sorted_by: Optional[Iterable[tuple]]) -> Optional[dict]:
+    if sorted_by is None:
+        sorted_by = []
+
+    try:
+        last_item = query_result[-1]
+    except IndexError:
+        return None
 
     if '_id' not in dict(sorted_by):
         sorted_by += [('_id', pymongo.ASCENDING)]
@@ -54,9 +49,14 @@ def get_next_cursor(query_result: list, options: PaginationOptions) -> dict:
     return next_query
 
 
-def get_previous_cursor(query_result: list, options: PaginationOptions):
-    first_item = query_result[0]
-    sorted_by = options.sort
+def get_previous_cursor(query_result: list, sorted_by: Optional[Iterable[tuple]]) -> Optional[dict]:
+    if sorted_by is None:
+        sorted_by = []
+
+    try:
+        first_item = query_result[0]
+    except IndexError:
+        return None
 
     if '_id' not in dict(sorted_by):
         sorted_by += [('_id', pymongo.ASCENDING)]
@@ -73,20 +73,35 @@ def get_previous_cursor(query_result: list, options: PaginationOptions):
     return prev_query
 
 
-def find(collection: Collection, query: dict, options: PaginationOptions) -> PaginatedResult:
-    cursor = collection.find({
-        '$and': [get_pagination_query(options), query]
-    }).limit(options.limit)
+def find(
+    collection: Collection,
+    query: dict,
+    limit: int,
+    next_cursor: Optional[str] = None,
+    prev_cursor: Optional[str] = None,
+    sort: Optional[Iterable[tuple]] = None,
+    projection: Optional[dict] = None
+) -> PaginatedResult:
+    query = {
+        '$and': [get_pagination_query(next_cursor, prev_cursor), query]
+    }
+    # query 1 more than we need just to see if there are more pages
+    cursor = collection.find(query, projection).limit(limit + 1)
 
     items = list(cursor)
+    has_next = len(items) > limit
+
+    # remove the last item from the results list since we queried more than requested
+    if has_next:
+        items.pop()
+
+    # create new set of cursors
+    next_cursor = get_next_cursor(items, sort) if has_next else None
+    prev_cursor = get_previous_cursor(items, sort)
 
     return PaginatedResult(
         data=items,
-        next=encode_cursor(get_next_cursor(items, options)),
-        previous=encode_cursor(get_next_cursor(items, options)),
-        has_next=True  # todo
+        next=encode_cursor(next_cursor) if next_cursor is not None else None,
+        previous=encode_cursor(prev_cursor) if prev_cursor is not None else None,
+        has_next=has_next
     )
-
-
-
-
